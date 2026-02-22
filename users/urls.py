475410c978957a72cfab200f2d8ea1ecd09c279b
zRedHox <30298730+zRedHox@users.noreply.github.com>
2026-02-23 00:02:23 +0700

    Fix formatting in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import users_collection
 
 urlpatterns = [
-    path("users", users_collection),  # no trailing slash to match requirement
+    path("users", users_collection),  
 ]
